0cf21997b9c01a7fa0d6db9deb5d2f46e3aab23c
Alex Aredov <59872895+AlexAredov@users.noreply.github.com>
2021-10-23 23:33:43 +0300

    a

--- main.c
+++ main.c
@@ -1,16 +1,16 @@
 #include <Windows.h>
 #include <wincon.h>
 #include <stdio.h>
+#include <string.h>
 
 void add(char p[1000]) {
     FILE* Pre;
     char str[100] = "";
     printf("Enter prefix: ");
     scanf_s("%s", &str, 100);
-
-    char str1[110];
+    char str1[10000];
     fopen_s(&Pre, p, "r");
-    fgets(&str1, 255, Pre);
+    fgets(&str1, 10000, Pre);
     //printf("%s", str1);
     fclose(Pre);
 
@@ -203,8 +203,161 @@ void menu_e() {
     }
 }
 
-void razbor(){
+void strrem(char* _pSourceStr, const char* _pDelStr )
+{
+    char* pTmpStr;
+    pTmpStr = strstr( _pSourceStr, _pDelStr );
+    //printf("%s\n", pTmpStr);
+    strcpy( pTmpStr, pTmpStr + strlen(_pDelStr) );
+    //printf("%s, %d", pTmpStr, strlen(_pDelStr));
+}
 
+void del(char sss[], char strr[], int k, char* str){
+    char **res  = NULL;
+    char *p    = strtok(sss, " ");
+    int n_spaces = 0;
+
+    while (p) {
+        res = realloc(res, sizeof (char*) * ++n_spaces);
+        if (res == NULL)
+            exit (-1);
+
+        res[n_spaces-1] = p;
+        p = strtok (NULL, " ");
+    }
+
+    res = realloc (res, sizeof (char*) * (n_spaces+1));
+    res[n_spaces] = 0;
+    char *istr;
+    int d = 0;
+
+    for (int i = 0; i < n_spaces; i++) {
+        istr = strstr (strr,res[i]);
+        if ( istr == NULL) {
+            //printf("NO\n");
+        }
+        else {
+            //printf("res[%d] = %s\n", i, res[i]);
+            //printf("YES %d %d\n", istr - strr + 1, strlen(strr) - 3);
+            if (istr - strr == 0 && k == 1){
+                printf("Prefix: %s\n", res[i]);
+
+                strrem(strr, res[i]);
+                str = strr;
+
+                break;
+            }
+            if (istr - strr > strlen(strr) - 4 && k == 4){
+                //printf("%d", istr - strr + 1 + strlen(res[i]));
+
+                if (strcmp(res[i], "s") != 0){
+                    printf("Ending: %s\n", res[i]);
+
+                    strrem(strr, res[i]);
+                    str = strr;
+
+                    break;
+                }
+                else{
+                    if (istr - strr == strlen(strr)){
+                        printf("Ending: %s\n", res[i]);
+
+                        strrem(strr, res[i]);
+                        str = strr;
+
+                        break;
+                    }
+                }
+
+
+
+            }
+            if (k == 3){
+
+                if (strcmp(res[i], "age") != 0 && strcmp(res[i], "ant") != 0){
+                    printf("Suffix: %s\n", res[i]);
+                    strrem(strr, res[i]);
+                    str = strr;
+                }
+                else{
+                    if (strcmp(str, "age") != 0 && strcmp (str, "ant") != 0){
+                        printf("Suffix: %s\n", res[i]);
+                        strrem(strr, res[i]);
+                        str = strr;
+                    }
+                    else{
+                        str = strr;
+                        break;
+                    }
+
+                }
+            }
+            if (k == 2){
+                if (strcmp(strr, res[i])==0){
+                    d++;
+                }
+            }
+        }
+    }
+    if (k == 2){
+        if (d == 0){
+            FILE* rot;
+            fopen_s(&rot, "C:\\Users\\aared\\CLionProjects\\untitled\\Root.txt", "a");
+            //printf("%s\n", strcat(strr, " "));
+            fprintf(rot, "%s", strcat(strr, " "));
+            printf("Root: %s\n", strr);
+            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_GREEN);
+            printf("*the root was not found in the database and was entered into it*\n");
+            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_BLUE | FOREGROUND_RED | FOREGROUND_GREEN);
+            fclose(rot);
+        }
+        else {
+            printf("Root: %s\n", strr);
+        }
+    }
+}
+
+void razbor(){
+    FILE* Pre;
+    char pre[110];
+    fopen_s(&Pre, "C:\\Users\\aared\\CLionProjects\\untitled\\Pre.txt", "r");
+    fgets(&pre, 255, Pre);
+
+    FILE* SSS;
+    char sufff[1100];
+    fopen_s(&SSS, "C:\\Users\\aared\\CLionProjects\\untitled\\Suff.txt", "r");
+    fgets(&sufff, 255, SSS);
+
+    FILE* Root;
+    char rt[100000];
+    fopen_s(&Root, "C:\\Users\\aared\\CLionProjects\\untitled\\Root.txt", "r");
+    fgets(&rt, 100000, Root);
+
+    FILE* End;
+    char end[110];
+    fopen_s(&End, "C:\\Users\\aared\\CLionProjects\\untitled\\End.txt", "r");
+    fgets(&end, 255, End);
+
+    char str[100] = "";
+    printf("Enter word: ");
+    scanf_s("%s", &str, 100);
+    printf("\n");
+
+    del(pre, str, 1, str);
+    del(end, str, 4, str);
+    del(sufff, str, 3, str);
+
+
+    //printf("%s\n", str);
+
+    fclose(Root);
+
+    del(rt, str, 2, str);
+    printf("\n");
+
+    fclose(Pre);
+    fclose(SSS);
+    fclose(End);
 }
 
 void menuu() {
