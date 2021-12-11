0d689f2424da6196286fe7c6bf2dd30c32eb4b2a
Alex Aredov <59872895+AlexAredov@users.noreply.github.com>
2021-12-11 22:55:26 +0300

    asdffhtym

--- 5.10.c
+++ 5.10.c
@@ -3,7 +3,23 @@
 //из строки в число
 int main()
 {
-    char *sss = "652"; //строка для преобразования
-    int n = atoi(sss); //преобразование строки в число типа int
-    printf("%d",n); //вывод результата преобразования
+    char sss[10];
+    scanf("%s", sss);
+    int pos = 1;
+    int i = 0;
+    if (sss[i] == '-'){
+        pos = 0;
+        i++;
+    }
+    int res = 0;
+    while (sss[i] != '\0'){
+        int k = sss[i] - '0';
+        res = res * 10 + k;
+        i++;
+    }
+    if (pos == 0){
+        res = res * -1;
+    }
+    printf("%d",res); //вывод результата преобразования
+    return 0;
 }
